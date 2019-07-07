46dfdad4004d7477ca5906128808c7ed7f9ba365
FangFeng077 <fangfeng077@gmail.com>
2019-07-07 00:36:06 -0700

    update

---
+++ Lecture1/KarelGoesHomeStarter.py
@@ -0,0 +1,15 @@
+from karel.stanfordkarel import *
+
+def main():
+    """
+    Pre-condition: Karel starts at the bottom left corner of the world,
+                   facing east
+    Post-condition: Karel has picked up the beeper representing "home"
+    """
+    # FILL ME IN!
+    pass
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
